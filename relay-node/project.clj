89f03e8a0763650776f15b16a4ce3d153ba97791
0xFORDCOMMA <comma@maxfieldlehman.com>
2019-03-18 16:17:49 -0700

    Add ubergraph dependency.

--- relay-node/project.clj
+++ relay-node/project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [ubergraph "0.5.2"]]
   :main ^:skip-aot relay-node.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
